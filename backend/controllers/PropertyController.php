8b9a8844bcce7a514b2b1b93cd98b3b264d67025
Niran Wangyad <Niran@cicgroup.net>
2017-11-24 22:31:40 +0700

    update

--- backend/controllers/PropertyController.php
+++ backend/controllers/PropertyController.php
@@ -23,7 +23,7 @@ class PropertyController extends Controller
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'delete' => ['POST'],
+                    'delete' => ['POST','GET'],
                 ],
             ],
         ];
--- console/migrations/m171124_142738_add_column_to_product_table.php
+++ console/migrations/m171124_142738_add_column_to_product_table.php
@@ -12,7 +12,7 @@ class m171124_142738_add_column_to_product_table extends Migration
      */
     public function safeUp()
     {
-        // $this->addColumn('product','type_id',$this->integer());
+         $this->addColumn('product','type_id',$this->integer());
         // $this->addColumn('product','brand_id',$this->integer());
         $this->addColumn('product','property_id',$this->integer());
     }
@@ -22,7 +22,7 @@ class m171124_142738_add_column_to_product_table extends Migration
      */
     public function safeDown()
     {
-        // $this->dropColumn('product','type_id');
+         $this->dropColumn('product','type_id');
         // $this->dropColumn('product','brand_id');
         $this->dropColumn('product','property_id');
     }
