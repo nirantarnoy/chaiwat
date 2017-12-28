24f1cc5e64abebf29bdad3283cc7abbc5de07c49
Niran Wangyad <Niran@cicgroup.net>
2017-12-28 08:53:48 +0700

    update

--- backend/views/product/_print.php
+++ backend/views/product/_print.php
@@ -12,6 +12,9 @@
 					<th style="text-align: left">กลุ่ม</th>
 					<th style="text-align: left">ประเภท</th>
 					<th style="text-align: left">คุณสมบัติ</th>
+					<th style="text-align: left">สั่งซื้อ</th>
+					<th style="text-align: right">ซื้อ</th>
+					<th style="text-align: right">ขาย</th>
 					<th style="text-align: right">จำนวน</th>
 					<th style="text-align: left">หน่วย</th>
 					<th style="text-align: right">ต้นทุน</th>
@@ -30,6 +33,9 @@
 					<td><?= \backend\models\Category::getCategorycode($value->category_id)?></td>
 					<td><?= \backend\models\Producttype::getTypename($value->type_id)?></td>
 					<td><?= \backend\models\Property::getPropertyname($value->property_id)?></td>
+					<td style="text-align: right"><?= $value->mode == 1?'Yes':'No'?></td>
+					<td style="text-align: right"><?=number_format($value->sale_qty,0)?></td>
+					<td style="text-align: right"><?=number_format($value->purch_qty,0)?></td>
 					<td style="text-align: right"><?=number_format($value->qty,0)?></td>
 					<td><?= \backend\models\Unit::getUnitname($value->unit_id)?></td>
 					<td style="text-align: right"><?=number_format($value->cost,0)?></td>
