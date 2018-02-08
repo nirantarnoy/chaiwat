12bafa70277a06a7a147055b6659c968083f95d6
Niran Wangyad <Niran@cicgroup.net>
2018-02-08 09:07:02 +0700

    update

--- backend/views/product/_print.php
+++ backend/views/product/_print.php
@@ -20,11 +20,10 @@
 					<th style="text-align: right">คงเหลือ</th>
 					<th style="text-align: right">คืน</th>
 					<th style="text-align: right">ปรับปรุง</th>
-					
-					<th style="text-align: right">ต้นทุน</th>
 					<th style="text-align: right">ทุนรวม</th>
+					<th style="text-align: right">ต้นทุน</th>
 					<th style="text-align: right">ผู้จำหน่าย</th>
-					<th style="text-align: left">หน่วย</th>
+					<!-- <th style="text-align: left">หน่วย</th> -->
 				</tr>
 			</thead>
 			<tbody>
@@ -61,7 +60,7 @@
 					<td style="text-align: right"><?=$cost_sum?></td>
 					<td style="text-align: right"><?=$cost?></td>
 					<td><?= \backend\models\Vendor::getVendorname($value->vendor_id)?></td>
-					<td><?= \backend\models\Unit::getUnitname($value->unit_id)?></td>
+					<!-- <td><?php //echo \backend\models\Unit::getUnitname($value->unit_id)?></td> -->
 				</tr>
 			<?php endforeach;?>
 		<?php endif;?>
