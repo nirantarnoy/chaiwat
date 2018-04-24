bb1570b312a4b1d6154914ce3698efc5d97dd299
Niran Wangyad <niran.w@cicnetgrp.net>
2018-04-24 16:54:44 +0700

    update

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -130,7 +130,7 @@ class ProductController extends Controller
                      ->andFilterWhere(['like','product_code',$session['code_search']])
                      ->andFilterWhere(['like','name',$session['text_search']]);
         if($movement2 == 0 && $movement2 !=''){
-             $dataProvider->query->andFilterWhere(['sale_qty'=>0])->andFilterWhere(['purch_qty'=>0]);
+             $dataProvider->query->andFilterWhere(['and',['sale_qty'=>0],['purch_qty'=>0]);
         }else if($movement2 == 1 && $movement2 !=''){
              $dataProvider->query->andFilterWhere(['or',['>','sale_qty',0],['>','purch_qty',0]]);
         }
